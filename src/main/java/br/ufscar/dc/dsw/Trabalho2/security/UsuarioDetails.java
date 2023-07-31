508bee1fee49ecc543fd5403ed60a8e7606a16e1
Lucas Balieiro <lucas.balieiro@estudante.ufscar.br>
2023-07-31 01:14:22 -0300

    Fix UsuarioDetails

--- src/main/java/br/ufscar/dc/dsw/Trabalho2/security/UsuarioDetails.java
+++ src/main/java/br/ufscar/dc/dsw/Trabalho2/security/UsuarioDetails.java
@@ -2,7 +2,6 @@ package br.ufscar.dc.dsw.Trabalho2.security;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -23,7 +22,7 @@ public class UsuarioDetails implements UserDetails {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         SimpleGrantedAuthority authority = new SimpleGrantedAuthority(usuario.getTipo());
-        return List.of(authority);
+        return Arrays.asList(authority);
     }
 
 	@Override
