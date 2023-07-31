c7a24e89a464d5a116b220e30d363533600cfb24
Lucas Balieiro <lucas.balieiro@estudante.ufscar.br>
2023-07-30 22:51:44 -0300

    Fix WebSecurityConfig

--- src/main/java/br/ufscar/dc/dsw/Trabalho2/config/WebSecurityConfig.java
+++ src/main/java/br/ufscar/dc/dsw/Trabalho2/config/WebSecurityConfig.java
@@ -1,58 +1,52 @@
 package br.ufscar.dc.dsw.Trabalho2.config;
 
-import org.springframework.context.annotation.*;
-import org.springframework.security.authentication.dao.*;
-import org.springframework.security.config.annotation.authentication.builders.*;
-import org.springframework.security.config.annotation.web.builders.*;
-import org.springframework.security.config.annotation.web.configuration.*;
-import org.springframework.security.core.userdetails.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 import br.ufscar.dc.dsw.Trabalho2.security.UsuarioDetailsServiceImpl;
 
 @Configuration
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig{
 
-	@Bean
-	public UserDetailsService userDetailsService() {
-		return new UsuarioDetailsServiceImpl();
-	}
+  @Bean
+  public UserDetailsService userDetailsService() {
+    return new UsuarioDetailsServiceImpl();
+  }
 
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
-	@Bean
-	public DaoAuthenticationProvider authenticationProvider() {
-		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-		authProvider.setUserDetailsService(userDetailsService());
-		authProvider.setPasswordEncoder(passwordEncoder());
+  @Bean
+  public DaoAuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(userDetailsService());
+    authProvider.setPasswordEncoder(passwordEncoder());
 
-		return authProvider;
-	}
-	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.authenticationProvider(authenticationProvider());
-	}
+    return authProvider;
+  }
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				.antMatchers("/", "/index", "/error","/carros/listar").permitAll()
-				.antMatchers("/propostas/**").hasAnyAuthority("CLIENTE","LOJA")
-				.antMatchers("/usuarios/**", "/admin/**","/admins/**").hasAuthority("ADMIN")
-				.antMatchers("/lojas/**", "/carros/**").hasAnyAuthority("LOJA","ADMIN")
-				.antMatchers("/login/**", "/js/**", "/css/**", "/image/**", "/webjars/**").permitAll()
-				.anyRequest().authenticated()
-			.and()
-				.formLogin()
-				.permitAll()
-			.and()
-				.logout()
-				.logoutSuccessUrl("/")
-				.permitAll();
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+		.authorizeHttpRequests(authz -> authz
+				.requestMatchers("/", "/index", "/error").permitAll()
+				.requestMatchers("/login/**", "/site/listar", "/site/lista", "/image/**", "/hotel/listar","/hotel/lista").permitAll()
+				.requestMatchers("/admin/**").hasRole("ADMIN")
+				.requestMatchers("/hotel/**").hasRole("HOTEL")
+				.requestMatchers("/site/**").hasRole("SITE")
+				.anyRequest().authenticated())
+		.formLogin(login -> login.loginPage("/login").permitAll())
+		.logout((logout) -> logout.logoutSuccessUrl("/").permitAll());
+		
+		return http.build();
 	}
 }
