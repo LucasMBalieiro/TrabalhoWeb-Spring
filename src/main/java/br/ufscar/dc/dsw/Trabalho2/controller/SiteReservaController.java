8eaa99fca630db42b8b9d30172ffacc83673cd9e
Ast <vitor.orsin@estudante.ufscar.br>
2023-07-31 13:31:56 -0300

    date validation

--- src/main/java/br/ufscar/dc/dsw/Trabalho2/controller/SiteReservaController.java
+++ src/main/java/br/ufscar/dc/dsw/Trabalho2/controller/SiteReservaController.java
@@ -33,49 +33,6 @@ public class SiteReservaController {
 	@Autowired
 	private IHotelService hservice;
 
-//	@GetMapping(value={"/home","/"})
-//	public String index1() {
-//		return "hotel/index";
-//	}
-//	@GetMapping("/cadastrar")
-//	public String cadastrar(SiteReserva siteReserva) {
-//		return "site/cadastro";
-//	}
-	
-//	@GetMapping("/listar")
-//	public String listar(ModelMap model) {
-//		model.addAttribute("sitereserva",sservice.buscarTodos());
-//		return "Site/listaPromocoes";
-//	}
-	
-//	@PostMapping("/salvar")
-//	public String salvar(@Valid SiteReserva siteReserva, BindingResult result, RedirectAttributes attr) {
-//
-//		if (result.hasErrors()) {
-//			return "site/cadastro";
-//		}
-//
-//		System.out.println("password = " + siteReserva.getSenha());
-//
-//		siteReserva.setSenha(siteReserva.getSenha());
-//		sservice.salvar(siteReserva);
-//		attr.addFlashAttribute("sucess", "site.create.sucess");
-//		return "redirect:/site/listar";
-//	}
-	
-//	@GetMapping("/editar/{id}")
-//	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
-//		model.addAttribute("sitereserva", sservice.buscarPorId(id));
-//		return "site/cadastro";
-//	}
-//
-//	@GetMapping("/excluir/{id}")
-//	public String excluir(@PathVariable("id") Long id, ModelMap model) {
-//		sservice.excluir(id);
-//		model.addAttribute("sucess", "site.delete.sucess");
-//		return listar(model);
-//	}
-
 	@GetMapping("/listarPromocoes")
 	public String listarPromocoes(ModelMap model) {
 		List<Promocao> l1 = pservice.buscarTodosPorHotel(getURLAtual());
